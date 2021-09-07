139aa794e0ce92ff60932763a4b1412878631466
Casey Harding <casey.harding@generalassemb.ly>
2021-09-07 13:22:18 -0400

    Onwards and upwards.

--- src/components/PetsList.js
+++ src/components/PetsList.js
@@ -3,12 +3,15 @@ import React from 'react'
 import Pet from './Pet';
 
 export default function PetsList(props) {
-  const { pets } = props
-
+  const { pets } = props;
+  // props.pets
   return (
     <div className='list-pets-friends container'>
       {/* 2- Loop over the data generating a Pet element as you go */}
       {/* What props does the Pet component expect? */}
+      {pets.map(myPet => {
+        return <Pet />
+      })}
     </div>
   )
 }
